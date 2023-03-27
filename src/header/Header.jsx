65e5e2da958ed373a996965162d9b66f47331eab
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-27 23:34:28 +0200

    header v2

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>jsjj</div>;
+  return (
+    <header className="header">
+      <form className="header__form">
+        <input
+          type="text"
+          placeholder="Enter a city name"
+          className="header__input"
+        />
+        <button type="submit" className="header__button">
+          Search
+        </button>
+      </form>
+    </header>
+  );
 };
 
 export default Header;
