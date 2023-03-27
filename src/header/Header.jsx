8af010fba799d064ec14852addcf50ed407b37ce
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-28 00:07:39 +0200

    header done

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,6 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
+      <h1 className="header__title">Weather App</h1>
       <form className="header__form">
         <input
           type="text"
